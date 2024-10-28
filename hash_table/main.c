83999af16e7490dd64deb24afbe0a7487cb15fdb
Mateus Pitura <119008106+MateusPitura@users.noreply.github.com>
2024-10-28 16:26:49 +0000

    feat: added hash table with rehash

--- hash_table/main.c
+++ hash_table/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SIZE 20
+#define SIZE 10
+#define TRUE 1
+#define FALSE 0
 
 struct node
 {
@@ -13,43 +15,46 @@ struct node
 struct node *BUCKETS[SIZE];
 
 int generateHash(int key);
-void insert(int key, int value);
+int regenerateHash(int position);
+void insert(int key, int value, int rehash);
 void search(int key);
 void print(void);
 
 int main(void)
 {
     print();
-    insert(23, 45);
-    insert(78, 92);
-    insert(34, 56);
-    insert(12, 89);
-    insert(67, 49);
-    insert(91, 15);
-    insert(40, 73);
-    insert(29, 58);
-    insert(86, 33);
-    insert(50, 20);
-    insert(14, 67);
-    insert(33, 90);
-    insert(55, 21);
-    insert(3, 78);
-    insert(47, 12);
-    insert(65, 84);
-    insert(23, 99);
-    insert(7, 54);
-    insert(88, 42);
-    insert(10, 76);
-    insert(59, 32);
-    insert(26, 11);
-    insert(97, 45);
-    insert(16, 63);
-    insert(82, 9);
-    insert(44, 69);
-    insert(31, 50);
-    insert(75, 18);
-    insert(92, 37);
-    insert(6, 81);
+    insert(23, 45, TRUE);
+    insert(78, 92, TRUE);
+    insert(34, 56, TRUE);
+    insert(12, 89, TRUE);
+    insert(67, 49, TRUE);
+    insert(91, 15, TRUE);
+    insert(40, 73, TRUE);
+    insert(29, 58, TRUE);
+    insert(86, 33, TRUE);
+    insert(50, 20, TRUE);
+    insert(14, 67, TRUE);
+    insert(33, 90, TRUE);
+    insert(55, 21, TRUE);
+    insert(3, 78, TRUE);
+    /*
+    insert(47, 12, TRUE);
+    insert(65, 84, TRUE);
+    insert(23, 99, TRUE);
+    insert(7, 54, TRUE);
+    insert(88, 42, TRUE);
+    insert(10, 76, TRUE);
+    insert(59, 32, TRUE);
+    insert(26, 11, TRUE);
+    insert(97, 45, TRUE);
+    insert(16, 63, TRUE);
+    insert(82, 9, TRUE);
+    insert(44, 69, TRUE);
+    insert(31, 50, TRUE);
+    insert(75, 18, TRUE);
+    insert(92, 37, TRUE);
+    insert(6, 81, TRUE);
+    */
     print();
     search(20);
     search(25);
@@ -61,16 +66,41 @@ int generateHash(int key)
     return key % SIZE; // Irá gerar valores de 0 a 9
 }
 
-void insert(int key, int value)
+void insert(int key, int value, int rehash)
 {
     struct node *newElement = (struct node *)malloc(sizeof(struct node));
     newElement->key = key;
     newElement->value = value;
     newElement->next = NULL;
     int hash = generateHash(key);
-    struct node *oldElement = BUCKETS[hash];
-    newElement->next = oldElement;
-    BUCKETS[hash] = newElement;
+    if (rehash == FALSE)
+    {
+        struct node *oldElement = BUCKETS[hash];
+        newElement->next = oldElement;
+        BUCKETS[hash] = newElement;
+    }
+    else if (rehash == TRUE)
+    {
+        int count = 0;
+        while (BUCKETS[hash] != NULL && count < SIZE)
+        {
+            count++;
+            hash = regenerateHash(hash);
+        }
+        if (count < SIZE)
+        {
+            BUCKETS[hash] = newElement;
+        }
+        else
+        {
+            printf("Buckets full. Key %d, value %d not inserted\n", key, value);
+        }
+    }
+}
+
+int regenerateHash(int position)
+{
+    return (position + 1) % SIZE;
 }
 
 void search(int key)
